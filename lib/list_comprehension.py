aaf1b76df481ec3031d018e4d18cb707a606279d
Dean14692-prog <dennismuasyangui@gmail.com>
2025-05-13 23:37:38 +0300

    commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list=[n for n in num_list if n % 2 == 0]
+    return even_list
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
+
 
 def make_exclamation(sentence_list):
+    result = []
+    for sentence in sentence_list:
+        result.append(sentence + '!')
+    return result
+    
     pass
